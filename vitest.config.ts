6d14753673422ae6b4af5e47c918e2d69caa923b
Bryson <bryson@streamliners.dev>
2025-06-26 23:45:20 -0700

    Move test setup to test directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test-setup.ts'],
+    setupFiles: ['./test/test-setup.ts'],
     include: ['src/**/*.test.ts'],
   },
+  resolve: {
+    conditions: ['browser'],
+    alias: {
+      $lib: resolve(__dirname, './src')
+    }
+  }
 });
